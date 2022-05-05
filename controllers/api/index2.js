e76dc97059c80e91ab543e09eda19d07e5ecc8ab
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-05-04 20:42:58 -0700

    functions

--- controllers/api/index2.js
+++ controllers/api/index2.js
@@ -1,12 +1,13 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 const questions = [ 
-   { id: 1, name: 'question1'},
-   { id: 2, name: 'question2'},
-   { id: 3, name: 'question3'},
+   { id: 1, name: 'riddle1'},
+   { id: 2, name: 'riddle2'},
+   { id: 3, name: 'riddle3'},
 ];
 
 app.get('/', (req, res) => {
@@ -15,6 +16,28 @@ app.get('/', (req, res) => {
 
 app.get('/api/riddles', (req, res) => {
     res.send([1,2,3]);
+});
+
+app.post('/api/riddles', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).require()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+    if (!req.body.name || req.body.name.length < 3) {
+        //400 bad request
+        res.status(400).send('Answer is require to continue.');
+        return;
+    }
+    const question = {
+        id: question.length + 1,
+        name: req.body.name
+    };
+
+questions.push(question);
+res.send(question);
+
 });
 
 //PORT

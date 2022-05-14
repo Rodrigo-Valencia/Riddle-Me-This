57d15f9c2b0262280fd526aa684da030a654b0e4
Rodrigo <rodvalenciaa@gmail.com>
2022-05-14 11:33:08 -0700

    troubleshooting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const db = require('./models')
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(db);
-app.use(routes);
+// app.use(routes);
 
 
 // sequelize.sync({ force: false }).then(() => {

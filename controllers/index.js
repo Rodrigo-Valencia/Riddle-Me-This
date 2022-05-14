17ea131afb5b4dbf3cc8ccf21a4d8c1781841e31
Rodrigo <rodvalenciaa@gmail.com>
2022-05-14 11:35:11 -0700

    troubleshooting

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const mainpageRoutes = require('./mainpage-route.js');
 
 router.use('/', mainpageRoutes)
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;

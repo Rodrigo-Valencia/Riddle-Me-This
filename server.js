6360743b3a6f3e5653889fbd7ef136a3837d5caa
Rodrigo <rodvalenciaa@gmail.com>
2022-05-14 18:00:57 -0700

    troubleshooting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const db = require('./models')
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -36,6 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('========NOW LISTENING========'));
 });

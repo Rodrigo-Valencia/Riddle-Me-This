86c654a2ebde5cedc130df07048a9b9c2f2c2520
Rodrigo <rodvalenciaa@gmail.com>
2022-05-14 18:46:57 -0700

    troubleshooting

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let User = require('../../models/User')
+let User = require('../../models')
 // CREATE new user
 router.post('/', async (req, res) => {
   try {

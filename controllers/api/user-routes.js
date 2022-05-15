c2ef4387622489ab65c4e95bdb28c842c07f1980
Rodrigo <rodvalenciaa@gmail.com>
2022-05-14 18:23:21 -0700

    troubleshooting

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const { User } = require('./models');
+const { User } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
-  console.log('Route Hit')
   try {
     const dbUserData = await User.create({
       username: req.body.username,

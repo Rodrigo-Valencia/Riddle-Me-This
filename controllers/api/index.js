36a21eb30a2972cb92f5b33bcad0b2a8400ab4a3
defhook <defhook152@gmail.com>
2022-05-15 14:53:16 -0700

    pushing update to main

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+//const userRoutes = require('./user-routes');
 // const riddleRoutes = require('./riddleRoutes');
 
 // router.use('/riddles', riddleRoutes);
